0d336808e7069ebc10816ca69cc43f9e098b8095
Sean <sean.snow@live.com>
2018-01-12 13:33:31 +0800

    删除无用的代码

--- feign/src/main/java/feign/AppReflectiveFeign.java
+++ feign/src/main/java/feign/AppReflectiveFeign.java
@@ -74,55 +74,6 @@ public class AppReflectiveFeign extends Feign {
         return proxy;
     }
 
-    static class FeignInvocationHandler implements InvocationHandler {
-
-        private final Target target;
-        private final Map<Method, MethodHandler> dispatch;
-
-        FeignInvocationHandler(Target target, Map<Method, MethodHandler> dispatch) {
-            this.target = checkNotNull(target, "target");
-            this.dispatch = checkNotNull(dispatch, "dispatch for %s", target);
-        }
-
-        @Override
-        public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-            if ("equals".equals(method.getName())) {
-                try {
-                    Object
-                            otherHandler =
-                            args.length > 0 && args[0] != null ? Proxy.getInvocationHandler(args[0]) : null;
-                    return equals(otherHandler);
-                } catch (IllegalArgumentException e) {
-                    return false;
-                }
-            } else if ("hashCode".equals(method.getName())) {
-                return hashCode();
-            } else if ("toString".equals(method.getName())) {
-                return toString();
-            }
-            return dispatch.get(method).invoke(args);
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if (obj instanceof FeignInvocationHandler) {
-                FeignInvocationHandler other = (FeignInvocationHandler) obj;
-                return target.equals(other.target);
-            }
-            return false;
-        }
-
-        @Override
-        public int hashCode() {
-            return target.hashCode();
-        }
-
-        @Override
-        public String toString() {
-            return target.toString();
-        }
-    }
-
     static final class ParseHandlersByName {
 
         private final Contract contract;
